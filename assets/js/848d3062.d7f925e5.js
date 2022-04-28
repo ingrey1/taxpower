d535e9adac74ca0415426e0d05b0631b038ab74c
Jason Mortensen <ingrey1@gmail.com>
2022-04-28 12:08:40 -0700

    Deploy website - based on ce228887b1b8af08fdacc9e129ec7dbd18bf024a

---
+++ assets/js/848d3062.d7f925e5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunktaxpower=self.webpackChunktaxpower||[]).push([[889],{3769:function(e){e.exports=JSON.parse('{"plugin":{"name":"docusaurus-plugin-content-docs","id":"default"}}')}}]);
